02afd40b05a3cde7ba2c7e39c5c353dcb5dd1bfd
nguyennd-0051 <nguyen.duy.nguyen@sun-asterisk.com>
2020-02-10 09:43:50 +0700

    Finish chapter 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root 'application#hello'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /vi|en/ do
+    root "static_pages#home"
+    
+    get "static_pages/home"
+    get "static_pages/help"
+    get "static_pages/about"
+    get "static_pages/contact"
+    end
 end
